f0e254fff6cbbed83eb527cd05e3cd752f0437e1
limingliang <darth2020>
2023-03-15 15:13:53 +0800

    修改启动脚本

--- tiklab-xcode-starter/bin/shutdown.sh
+++ tiklab-xcode-starter/bin/shutdown.sh
@@ -9,7 +9,7 @@ if [ -e "${DIRS}/${JDK_VERSION}" ]; then
 fi
 
 #APP_MAIN=${application.main.class}
-APP_MAIN="net.tiklab.xcode.XcodeApplication"
+APP_MAIN="io.tiklab.xcode.XcodeApplication"
 
 PID=0
 getPID(){
--- tiklab-xcode-starter/bin/status.sh
+++ tiklab-xcode-starter/bin/status.sh
@@ -8,7 +8,7 @@ if [ -e "${DIRS}/${JDK_VERSION}" ]; then
       JAVA_HOME="${DIRS}/${JDK_VERSION}"
 fi
 
-APP_MAIN="net.tiklab.xcode.XcodeApplication"
+APP_MAIN="io.tiklab.xcode.XcodeApplication"
 
 PID=0
 getPID(){
