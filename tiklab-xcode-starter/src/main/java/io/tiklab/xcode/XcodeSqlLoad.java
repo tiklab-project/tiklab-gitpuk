774bcf47afd2ed8edb75cb4a72dd39a7293267f1
limingliang <darth2020>
2023-04-12 14:08:56 +0800

    sql执行脚本修改调整

--- tiklab-xcode-starter/src/main/java/io/tiklab/xcode/XcodeSqlLoad.java
+++ tiklab-xcode-starter/src/main/java/io/tiklab/xcode/XcodeSqlLoad.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class xcodeSqlLoad {
+public class XcodeSqlLoad {
 
     @Bean
     DsmConfig initDsmConfig() {
